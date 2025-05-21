9a7c652169644af8c38852c07c4d1bcfca863f15
suhani954 <suhani20080064@gmail.com>
2025-05-21 16:40:59 +0530

    Add files via upload

---
+++ suhu_photo.js
@@ -0,0 +1,12 @@
+// script.js
+
+function toggleMenu() {
+  const navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
+
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  alert("Thank you for reaching out! We'll get back to you soon.");
+  this.reset();
+});
